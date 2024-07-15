a17a76dcef7a48baf94478bc677ba29b829d6238
Cpaluszek <paluszek.clement@gmail.com>
2024-07-15 18:17:53 +0200

    remove comment

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -3,9 +3,6 @@
 void Utils::signalHandler(int signal) {
     TintinReporter::getInstance().log(LOGLEVEL_WARN, "Interrupt signal (" + std::to_string(signal) + ") received.");
     MattDaemon::getInstance().shutdownRequested = true;
-    // MattDaemon& daemon = MattDaemon::getInstance();
-    // daemon.deleteLockFileAndCloseSocket();
-    // TintinReporter::getInstance().log(LOGLEVEL_INFO, "Matt_Daemon is shutting down.");
     std::exit(signal);
 }
 
